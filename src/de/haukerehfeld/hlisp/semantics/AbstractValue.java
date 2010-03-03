9303f7ea21fe63294981712b4b5d0978cdaff5e7
Hauke Rehfeld <hrehfeld@uni-koblenz.de>
2010-03-03 18:58:53 +0100

    chg parsing works

--- src/de/haukerehfeld/hlisp/semantics/AbstractValue.java
+++ src/de/haukerehfeld/hlisp/semantics/AbstractValue.java
@@ -2,62 +2,115 @@ package de.haukerehfeld.hlisp.semantics;
 
 import java.util.*;
 
-public abstract class AbstractValue implements Value {
-	public AbstractValue(Type type, Value scope) {
-		this.type = type;
-		this.scope = scope;
+import de.haukerehfeld.hlisp.Utils;
+
+public abstract class AnonymousType implements Type {
+	public AnonymousType(Type parent, Type returnType) {
+		this(parent, returnType, false);
 	}
 
-	private Type type;
-	@Override public Type getType() { return type; }
-	public void setType(Type type) { this.type = type; }
+	public AnonymousType(Type parent, Type returnType, boolean isFunction) {
+		this(parent, returnType, isFunction,
+		     Collections.<Type>emptyList(),
+		     Collections.<String>emptyList());
+	}
 	
-	private Value scope; 
-	@Override public Value getScope() { return scope; }
-	@Override public <T> T runOnScope(Value.ValueMethod<T> method) {
-		Value parent = this;
+	
+	public AnonymousType(Type parent,
+	                     Type returnType,
+	                     List<Type> parameterTypes,
+	                     List<String> parameterNames) {
+		this.parent = parent;
+		this.returnType = returnType;
+		this.parameterTypes = parameterTypes;
+		this.parameterNames = parameterNames;
+		this.isFunction = true;
+	}
+
+
+	private boolean isFunction;
+	public boolean isFunction() { return isFunction; }
+	public void setIsFunction(boolean isFunction) { this.isFunction = isFunction; }
+
+	private boolean isStatic = false;
+	public void setStatic(boolean isStatic) { this.isStatic = isStatic; }
+	@Override public boolean isStatic() { return isStatic; }
+
+	private boolean isPublic = false;
+	public void setPublic(boolean isPublic) { this.isPublic = isPublic; }
+	@Override public boolean isPublic() { return isPublic; }
+	
+	/** Parent type */
+	private final Type parent;
+	@Override public Type getParent() { return parent; }
+
+	/** params */
+	private List<Type> parameterTypes;
+	@Override public List<Type> getParameterTypes() { return parameterTypes; }
+	public void setParameterTypes(List<Type> parameterTypes) { this.parameterTypes = parameterTypes; }
+
+	/** return type */
+	private Type returnType;
+	@Override public Type getReturnType() { return returnType; }
+	public void setReturnType(Type returnType) { this.returnType = returnType; }
+
+	private final LinkedHashMap<String, Type> types = new LinkedHashMap<String, Type>();
+	@Override public List<Type> getDefinedTypes() { return types.values(); }
+	@Override public boolean isTypeDefined(String v) { return types.get(v) != null; }
+	@Override public void defineType(Type t) { types.put(t.getName(), t); }
+	@Override public Type getDefinedType(String name) { return types.get(name); }
+	@Override public <T> T runOnHierarchy(Type.TypeMethod<T> method) {
+		Type parent = this;
 		T result = null;
 		while (parent != null && !method.success()) {
 			result = method.run(parent);
-			parent = parent.getScope();
+			parent = parent.getParent();
 		}
 		if (!method.success()) {
 			return null;
 		}
 		return result;
 	}
-	@Override public boolean isMemberDefinedRecursive(final String name) {
-		return getDefinedMemberRecursive(name) != null;
+	@Override public boolean isTypeDefinedRecursive(final String name) {
+		return getDefinedTypeRecursive(name) != null;
 	}
-	@Override public Value getDefinedMemberRecursive(final String name) {
-		return runOnScope(new Value.ValueMethod<Value>() {
+	@Override public Value getDefinedTypeRecursive(final String name) {
+		return runOnHierarchy(new TypeMethod<Value>() {
 		        private boolean success = false;
 		        
-		        @Override public Value run(Value scope) {
+		        @Override public Type run(Type scope) {
 					//System.out.println("Searching " + scope + " for " + name);
 
-					if (scope.isMemberDefined(name)) {
+					if (scope.isTypeDefined(name)) {
 						success = true;
-						return scope.getDefinedMember(name);
+						return scope.getDefinedType(name);
 					}
 					return null;
 				}
 		        @Override public boolean success() { return success; }
-		    });		
+		    });
 	}
 
-	/** members */
-	private final LinkedHashMap<String, Value> members = new LinkedHashMap<String, Value>();
-	@Override public Map<String, Value> getDefinedMembers() {
-		return members;
+
+	@Override public String toString() {
+		StringBuilder name = new StringBuilder();
+		String self = getClass().getSimpleName();
+		name.append(self);
+		if (isFunction()) {
+			name.append(" (");
+			name.append(Utils.join(getParameterTypes(), " "));
+			name.append(" ->");
+		}
+		name.append(" ");
+		if (!getReturnType().equals(this)) {
+			name.append(self);
+		}
+		else {
+			name.append(getReturnType());
+		}
+		if (isFunction()) {
+			name.append(")");
+		}
+		return name.toString();
 	}
-	@Override public Value getDefinedMember(String name) {
-		return members.get(name);
-	}
-	@Override public boolean isMemberDefined(String v) {
-		return members.get(v) != null;
-	}
-	@Override public void defineMember(String name, Value v) { members.put(name, v); }
-	
-	@Override public String toString() { return getClass().getSimpleName(); }
 }
--- src/de/haukerehfeld/hlisp/semantics/Type.java
+++ src/de/haukerehfeld/hlisp/semantics/Type.java
@@ -5,6 +5,7 @@ import java.util.*;
 import de.haukerehfeld.hlisp.JavaEmitter;
 
 public interface Type {
+	public String getName();
 	public Type getParent();
 
 	public boolean isFunction();
@@ -12,17 +13,29 @@ public interface Type {
 	/** Can the type be instantiated? */
 	public boolean isStatic();
 
+	public boolean isPublic();
+
+	public <T> T runOnHierarchy(TypeMethod<T> m);
+	
 	/**
 	 * get parameters
 	 */
 	public List<Type> getParameterTypes();
+	public List<String> getParameterNames();
 
 	public Type getReturnType();
 
 	/** access types directly defined in this type */
-	public Map<String,Type> getDefinedTypes();
+	public List<Type> getDefinedTypes();
 	public Type getDefinedType(String name);
-	public boolean isTypeDefined(String v);
-	public void defineType(String name, Type v);
+	public Type getDefinedTypeRecursive(String name);
+	public boolean isTypeDefined(String name);
+	public boolean isTypeDefinedRecursive(String name);
+	public void defineType(Type v);
 
+	public interface TypeMethod<T> {
+		public T run(Type v);
+		public boolean success();
+	}
+	
 }
